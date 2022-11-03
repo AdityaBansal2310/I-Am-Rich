8b609d90ae1a2ec90cabb2f39c85be77e689cdc5
Aditya Bansal <103448377+AdityaBansal2310@users.noreply.github.com>
2022-11-03 23:13:39 +0530

    Add files via upload

---
+++ I am Rich/I am Rich/I am Rich/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  I am Rich
+//
+//  Created by Aditya Bansal on 03/11/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
